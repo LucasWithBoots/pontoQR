27e70a5dd3b17c9c5d7805f3475d65a0ee392e64
lucas ferrari <lucas.carrijoferrari@gmail.com>
2024-08-23 11:56:40 -0300

    mensagem de erro se não preencher ambos os campos

--- app/criarQRCode.tsx
+++ app/criarQRCode.tsx
@@ -1,7 +1,7 @@
 import InputComCaption from "@/components/InputComCaption";
 import { createQRCode } from "@/shared/service";
 import { ContextQRCodeCriado } from "@/store/context/context-qrcode-criado";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { useContext, useState } from "react";
 import {
   View,
@@ -15,17 +15,25 @@ export default function ScreenEspecificacoesQR() {
   const { qrCodes, setQRCodes } = useContext(ContextQRCodeCriado);
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
+  const [erro, setErro] = useState<string | null>("");
 
   const handleCriarQRCode = () => {
-    const novoQRCode = {
-      nome: nome,
-      descricao: descricao,
-      qrCode: `${nome}-${descricao}`, // Você pode substituir isso por um valor gerado ou recebido
-    };
+    if (nome && descricao) {
+      setErro(null);
+      const novoQRCode = {
+        nome: nome,
+        descricao: descricao,
+        qrCode: `${nome}-${descricao}`, // Você pode substituir isso por um valor gerado ou recebido
+      };
 
-    createQRCode(novoQRCode).then((resp) => {
-      setQRCodes((prev: any) => [...prev, resp]);
-    });
+      createQRCode(novoQRCode).then((resp) => {
+        setQRCodes((prev: any) => [...prev, resp]);
+      });
+
+      router.navigate("/QRCodeCriado");
+    } else {
+      setErro("É necessário preencher ambos os campos!");
+    }
   };
 
   return (
@@ -49,19 +57,20 @@ export default function ScreenEspecificacoesQR() {
         onChangeText={setDescricao}
         placeholder="PlaceHolder D"
       />
-      <Link href="/QRCodeCriado" asChild>
-        <TouchableOpacity
-          onPress={handleCriarQRCode}
-          className="bg-folly self-start px-5 py-3 rounded-full"
+
+      {erro ? <Text className="text-folly mb-5">{erro}</Text> : null}
+
+      <TouchableOpacity
+        onPress={handleCriarQRCode}
+        className="bg-folly self-start px-5 py-3 rounded-full"
+      >
+        <Text
+          className="color-white"
+          style={{ fontFamily: "SpaceGrotesk-Bold" }}
         >
-          <Text
-            className="color-white"
-            style={{ fontFamily: "SpaceGrotesk-Bold" }}
-          >
-            Criar
-          </Text>
-        </TouchableOpacity>
-      </Link>
+          Criar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
