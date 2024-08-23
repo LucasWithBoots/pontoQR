5a6bc23c9928cbd17446f245002fceb4ad29a1ff
lucas ferrari <lucas.carrijoferrari@gmail.com>
2024-08-23 09:39:22 -0300

    mudanças de estilos no qrcode criado

--- app/QRCodeCriado.tsx
+++ app/QRCodeCriado.tsx
@@ -4,13 +4,18 @@ import { View, Text } from "react-native";
 import QRCode from "react-native-qrcode-svg";
 
 export default function QRCodeCriado() {
-  const valorQRCode = useContext(ContextQRCodeCriado);
-
-  console.log(valorQRCode);
+  const { qrCodes, setQRCodes } = useContext(ContextQRCodeCriado);
+  const ultimoQRCode = qrCodes[qrCodes.length - 1];
 
   return (
-    <View>
-      <QRCode value="teste oioi" />
+    <View className="flex-1 justify-center items-center gap-y-4">
+      <QRCode value={ultimoQRCode.qrCode} />
+      <Text
+        className="text-white text-2xl"
+        style={{ fontFamily: "SpaceGrotesk-Medium" }}
+      >
+        Código criado com sucesso!
+      </Text>
     </View>
   );
 }
