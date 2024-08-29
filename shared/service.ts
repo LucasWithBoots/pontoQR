839d1ad86388b300b726940cc6a20e088f82bfa1
lucas ferrari <lucas.carrijoferrari@gmail.com>
2024-08-29 15:23:59 -0300

    feat: variavel com ip

--- shared/service.ts
+++ shared/service.ts
@@ -1,13 +1,15 @@
 import { QRCodeModelDAO } from "@/models/model.qrcode";
 
+const LOCAL_IP = "192.168.36.152";
+
 export const getQRCodes = () => {
-  return fetch("http://10.0.2.2:3000/qrCodes")
+  return fetch(`http://${LOCAL_IP}:3000/qrCodes`)
     .then((resp) => resp.json())
     .catch((error) => console.error(error));
 };
 
 export const createQRCode = (novoQRCode: QRCodeModelDAO) => {
-  return fetch("http://10.0.2.2:3000/qrCodes", {
+  return fetch(`http://${LOCAL_IP}:3000/qrCodes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
