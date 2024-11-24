f4226df9ab64e1f91ea2af760434b7dddb132e12
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-11-24 17:44:30 -0300

    initial commit

---
+++ backend/src/main/kotlin/io/github/lucaswithboots/Application.kt
@@ -0,0 +1,15 @@
+package io.github.lucaswithboots
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
